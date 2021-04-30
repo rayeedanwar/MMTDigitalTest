fba1e4248cb7f023515a322461b5129e9a2d37df
Rayeed _ <_rayeed@live.com>
2021-04-30 18:52:43 +0100

    Error message for login page

--- SauceDemo/Pages/Login.cs
+++ SauceDemo/Pages/Login.cs
@@ -7,6 +7,7 @@ namespace SauceDemoInteractionLibrary.Pages
         public IWebElement UserNameInput { get { return Driver.FindElement(By.XPath("//input[@id='user-name']")); } }
         public IWebElement PasswordInput { get { return Driver.FindElement(By.XPath("//input[@id='password']")); } }
         public IWebElement LoginButton { get { return Driver.FindElement(By.XPath("//input[@id='login-button']")); } }
+        public IWebElement ErrorMessage { get { return Driver.FindElement(By.XPath("//h3[@data-test='error']")); } }
 
         public Login(IWebDriver driver) : base(driver)
         {
