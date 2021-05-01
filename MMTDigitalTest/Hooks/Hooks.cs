7aadbe21d4ae2236200583a1b9ebccf4ef3fc9a6
Rayeed _ <_rayeed@live.com>
2021-05-01 17:31:34 +0100

    Added lighthouse results and different browser compatibility

--- MMTDigitalTest/Hooks/Hooks.cs
+++ MMTDigitalTest/Hooks/Hooks.cs
@@ -1,8 +1,14 @@
 ﻿using BoDi;
-using OpenQA.Selenium;
+using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Chrome;
 using SauceDemoInteractionLibrary;
 using TechTalk.SpecFlow;
+using System;
+using OpenQA.Selenium;
+using System.IO;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.IE;
 
 namespace MMTDigitalTest.Hooks
 {
@@ -19,7 +25,25 @@ namespace MMTDigitalTest.Hooks
         [BeforeScenario]
         public void SetupBrowser()
         {
-            var driver = new ChromeDriver();
+            var config = new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json", optional: true)
+                            // .AddEnvironmentVariables() if needed via pipelines
+                            .Build();
+
+            var selectedBrowser = config.GetSection("Browser").Value;
+            var driverDirectory = $"{Directory.GetCurrentDirectory()}/Drivers";
+            var options = new InternetExplorerOptions { EnableNativeEvents = false };
+            options.AddAdditionalCapability("disable-popup-blocking", true);
+
+            IWebDriver driver = selectedBrowser.ToLower() switch
+            {
+                "chrome" => new ChromeDriver(driverDirectory),
+                "firefox" => new FirefoxDriver(driverDirectory), // firefox driver is very slow - haven't investigated but test starts and executes
+                "edge" => new EdgeDriver(driverDirectory),
+                "ie11" => new InternetExplorerDriver(driverDirectory), // throws NoSuchWindowException after navigating to site
+                _ => throw new InvalidOperationException($"{selectedBrowser} not recognised in test suite")
+            };
+
             driver.Manage().Window.Maximize();
             // might be worth adding chromedriver to run from specific folder but pretty sure it runs with latest stable version because of Selenium.Webdriver? double check
             var sauceDemoApp = new SauceDemoApp(driver);
